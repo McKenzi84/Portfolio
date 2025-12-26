1d9b6afefdcef2bb9f44e4cb6df32721fbfc5410
McKenzi84 <smallbevelgear@gmail.com>
2025-12-26 12:33:16 +0100

    Change about

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,9 @@ import { defineConfig } from 'astro/config'
 export default defineConfig({
   site: 'https://mckenzi84.github.io',
   base: '/Portfolio/',
+  image: {
+    service: {
+      entrypoint: 'astro/assets/services/sharp'
+    }
+  }
 })
