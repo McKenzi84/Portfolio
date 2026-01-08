1e428ca5feb35054d975f8b4f2e0ac1f7e577db6
McKezni84 <smallbevelgear@gmail.com>
2026-01-08 14:15:05 +0100

    site url changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   base: '/Portfolio',
-  site: 'https://x-tom.online/Portfolio/',
+  site: 'https://x-tom.online/',
   integrations: [mdx(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
